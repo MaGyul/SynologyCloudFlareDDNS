6ebb1cf94b38e80b7e71ae040bc92ab4160d55ae
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2024-06-11 15:30:17 +0900

    Update setddns_router.py

--- setddns_router.py
+++ setddns_router.py
@@ -13,6 +13,8 @@ with open("/etc.defaults/ddns_provider.conf", "rw") as configFile:
                 data += "        queryurl=https://www.cloudflare.com/"
                 configFile.write(data)
                 print("write complete")
+        else:
+                print("exist data")
 
 urllib.request.urlretrieve(url, target_file)
 os.chmod(target_file, stat.S_IRUSR |  stat.S_IWUSR |  stat.S_IXUSR |  stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH)
